69dd00809cb0a830fd5826f9c5ce3fdd354f3980
szymonmat <bosik111@gmail.com>
2018-03-10 13:52:59 +0100

    Error handing for missing critital request elements added

---
+++ app/DomainModel/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel
+{
+    public class Category
+    {
+        public int Cid { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
--- app/EchoServer/Program.cs
+++ app/EchoServer/Program.cs
@@ -14,33 +14,36 @@ namespace EchoServer
         {
             var addr = IPAddress.Parse("127.0.0.1");
             var server = new TcpListener(addr, 5000);
-
+            List<Category> database = new List<Category>()
+            {
+                new Category { Cid = 1, Name = "Beverages"},
+                new Category { Cid = 2, Name = "Condiments" },
+                new Category { Cid = 3, Name = "Confections" }
+            };
             server.Start();
             Console.WriteLine("Server started ...");
 
 
             while (true)
             {
+                //connection setup
                 var client = server.AcceptTcpClient();
-
                 var strm = client.GetStream();
-
                 var buffer = new byte[client.ReceiveBufferSize];
                 var readCnt = strm.Read(buffer, 0, buffer.Length);
-
+                //Data processing setup
+                List<string> errorList = new List<string>();
                 var payload = Encoding.UTF8.GetString(buffer, 0, readCnt);
                 var request = JsonConvert.DeserializeObject<Request>(payload);
                 var response = new Response();
-                Console.WriteLine(request.Method);
                 String errorString = "";
-                List<string> errorList = new List<string>();
 
                 //Validate for mandatory elements (mehtod, date, path)
                 if (request.Method == null)
                 {
                     errorList.Add("missing body");
                 }
-                if (request.Date==0)
+                if (request.Date == 0)
                 {
                     errorList.Add("missing date");
                 }
@@ -48,7 +51,22 @@ namespace EchoServer
                 {
                     errorList.Add("missing path");
                 }
-
+                //If any of critical elements are missing generate error message, otherwise start evaluationg methods
+                if (errorList.Count > 0) {
+                    var l = errorList.Count;
+                    for (var i = 0; i < l; i++)
+                    {
+                        if (i != l)
+                        {
+                            string.Concat(errorString, errorList[i], ',');
+                        }
+                        else {
+                            string.Concat(errorString, errorList[i]);
+                        }
+                    }
+                    response.Status = "4 Bad Request";
+                    response.Body = errorString;
+                }
                 else
                 {
                     //Evaluate type of request, and execute appropriate code
